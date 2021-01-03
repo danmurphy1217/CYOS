7b853dee95cbf9eb388b82fa5c95c30668800b94
dan murphy <danielmurph8@gmail.com>
2021-01-03 18:01:28 -0500

    updated where startup_script.txt file is saved, added some more debugging, and cleaned up Stack YAML file to include a parameter fror UserData

---
+++ run_on_start.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo Initial Dir: $(pwd) > "startup_script.txt"
+cd home/ubuntu
+echo New Dir: $(pwd) >> "startup_script.txt"
+echo "running startup script" >> "startup_script.txt"
+sudo apt update -y
+sudo apt upgrade -y
+echo "updates complete." >> "startup_script.txt"
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt install nvidia-driver-435
+echo "installed nvidia driver." >> "startup_script.txt"
+wget https://developer.download.nvidia.com/compute/cuda/11.2.0/local_installers/cuda_11.2.0_460.27.04_linux.run
+sudo sh cuda_11.2.0_460.27.04_linux.run
+echo "installed CUDA" >> "startup_script.txt"
