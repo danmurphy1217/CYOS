66be7e416c47a838ed76595afaba4da537802e1b
dan murphy <danielmurph8@gmail.com>
2021-01-11 20:14:39 -0500

    setup complete for frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from "./pages/LoginPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Global } from "./styles/globals";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Global/>
+      <Switch>
+        <Route exact path='/login' component={LoginPage}/>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import LogIn from "../components/login";
+
+export default function LoginPage() {
+    return (
+        <div>
+            {LogIn()}
+        </div>
+    )
+}
