0a75749272b029bc47be3ab1bbe578bfc5871ed3
dan murphy <danielmurph8@gmail.com>
2021-01-16 21:36:33 -0500

    add redux middleware and configure devtools

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Global } from "./styles/globals";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore, applyMiddleware } from "redux";
+
+import appReducer from "./reducers";
+import reduxThunk from "redux-thunk";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+	appReducer,
+	composeWithDevTools(applyMiddleware(reduxThunk, logger))
+);
 
 function App() {
   return (
-    <Router>
-      <Global/>
-      <Switch>
-        <Route exact path='/login' component={LoginPage}/>
-        <Route exact path='/forgot' component={LoginPage}/>
-        <Route exact path='/signup' component={LoginPage}/>
-      </Switch>
-    </Router>
-  )
+    <Provider store={store}>
+      <Router>
+        <Global />
+        <Switch>
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/forgot" component={LoginPage} />
+          <Route exact path="/signup" component={LoginPage} />
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
